e6e33f49cfd5c903535f02a7380f065756c5be8b
kenanoestreich <kenano1999@gmail.com>
2022-07-25 14:04:46 -0500

    feat: solve problem 14

--- tutorialsPracticePython/listRemoveDuplicates.py
+++ tutorialsPracticePython/listRemoveDuplicates.py
@@ -1,3 +1,18 @@
 """This file takes in a list and returns a new list
 with the same contents as the first minus any duplicates"""
 
+def removeDuplicates(list) :
+    newlist = []
+    for num in list : 
+        if newlist.count(num)==0 :
+            newlist.append(num)
+    return newlist
+
+a = [1, 1, 2, 3, 4, 6, 6, 6, 7, 8, 9, 9, 9, 9, 10]
+
+b = removeDuplicates(a)
+print(b)
+
+
+
+
